e5afa7581ae64ffa1e27ccf71a49007314ea4040
Shuo Han <hanshuo@caltech.edu>
2010-12-06 17:38:02 -0800

    Corrected a bug on cummulative pose update

--- nodes/rigit_listener.py
+++ nodes/rigit_listener.py
@@ -18,8 +18,12 @@ class Listener:
         rospy.Subscriber('flydra_mainbrain_super_packets',
                          flydra_mainbrain_super_packet,
                          self.callback)
+        print 
+        print 'rigit_listener intialized.\n'
+        print 'Waiting input from flydra...'
         
         rospy.init_node('rigit_from_flydra', anonymous=True)
+
 
     def callback(self, super_packet):
         packet = super_packet.packets[0] # super_packet legacy
@@ -28,16 +32,22 @@ class Listener:
                                 for obj in packet.objects]).T
 
         if not self.is_initialized:
-            self.world_pts_init = self.world_pts
+            print 'Start receiving input from flydra. Publishing messages to \'pose estimation\'...'
+            self.world_pts_prev = self.world_pts
             self.is_initialized = True
+            self.R = eye(3)
+            self.T = zeros(3)
             
     def run(self):
         loop_rate = rospy.Rate(self.max_freq)
         
         while not rospy.is_shutdown():
             if self.is_initialized:
-                R, T, err = rigit_nn(self.world_pts_init, self.world_pts)
-                self.pub.publish(R.flatten().tolist(), T.tolist())
+                dR, dT, err = rigit_nn(self.world_pts_prev, self.world_pts)
+                self.R = dot(dR, self.R)
+                self.T = dot(dR, self.T) + dT
+                self.world_pts_prev = self.world_pts
+                self.pub.publish(self.R.flatten().tolist(), self.T.tolist())
 
             loop_rate.sleep()
 
